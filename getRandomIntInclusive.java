2520a6b98cfaf4906b83f193810e0bf8bd3a8b7d
= <rizard@lasalle.cat>
2023-01-27 10:48:41 +0100

    Adding getRandomNumberUsingNextInt

--- getRandomIntInclusive.java
+++
@@ -1,9 +0,0 @@
-/**
-* Credits: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-*/
-function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
-}
-
